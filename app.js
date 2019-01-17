1d307f814046103d0fbd0b6671acbf491fddf4a4
jbramble18 <jbramble.rls@gmail.com>
2019-01-16 23:00:23 -0600

    See-and-Say

--- app.js
+++ app.js
@@ -229,22 +229,23 @@ $("#yorkie").on("click", function () {
 
 
 
-var config = {
-  apiKey: "AIzaSyBtpr09-F0k89EO6_lAqt7zwP1iy4FVkW0",
-  authDomain: "see-n-say-14b3b.firebaseapp.com",
-  databaseURL: "https://see-n-say-14b3b.firebaseio.com",
-  projectId: "see-n-say-14b3b",
-  storageBucket: "see-n-say-14b3b.appspot.com",
-  messagingSenderId: "689086528373"
-};
-firebase.initializeApp(config);
+// var config = {
+//   apiKey: "AIzaSyBtpr09-F0k89EO6_lAqt7zwP1iy4FVkW0",
+//   authDomain: "see-n-say-14b3b.firebaseapp.com",
+//   databaseURL: "https://see-n-say-14b3b.firebaseio.com",
+//   projectId: "see-n-say-14b3b",
+//   storageBucket: "see-n-say-14b3b.appspot.com",
+//   messagingSenderId: "689086528373"
+// };
+// firebase.initializeApp(config);
 
-var database = firebase.database();
+// var database = firebase.database();
 
-function displayGifs() {
+$(".button").on('click', function (event)  {
 
 
-  var animal = $('#animal-input').val().trim();
+  var animal = $(this).attr("data-type");
+
   var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=DOIPJ1030YI69wiQkcqGIvkzMaK4C0Jh&limit=5";
 
   $.ajax({
@@ -271,11 +272,11 @@ function displayGifs() {
       }
     }
   })
-}
-
-$('#add-animal').on('click', function (event) {
-  event.preventDefault();
-  var userAni = $('#animal-input').val().trim();
-
-  displayGifs();
 });
+
+// $(".button").on('click', function (event) {
+//   event.preventDefault();
+  
+
+//   displayGifs();
+// });
